4b914017f2357769a4bd7e3c7be9c6ecac264485
Jayden Bailey <jaydenkieran@gmail.com>
2016-09-15 20:40:33 +0100

    Basic implementation of PlayerInfo class

--- smite/player.py
+++ smite/player.py
@@ -61,8 +61,44 @@ class PlayerInfo(Player):
     Represents a Smite player with additional information
 
     This class is a subclass of :class:`Player`
+
+    Attributes
+    ----------
+    status : str
+        The player's personal status message
+    region : str
+        The player's region
+    team_id : int
+        The player's clan ID. Could be None
+    team_name : str
+        The player's clan name. Could be None
+    avatar_url : str
+        The URL to the player's avatar
+    achievements : int
+        The total amount of achievements a player has
+    worshippers : int
+        The total amount of worshippers a player has
+    level : int
+        The level of the player
+    leaves : int
+        The amount of times the player left a game
+    losses : int
+        The amount of times the player lost a game
+    wins : int
+        The amount of times the player won a game
     """
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.status = kwargs.get('Personal_Status_Message')
+        self.region = kwargs.get('Region')
+        self.team_id = kwargs.get('TeamId')
+        self.team_name = kwargs.get('Team_Name')
+        self.avatar_url = kwargs.get('Avatar_URL')
+        self.achievements = kwargs.get('Total_Achievements')  # TODO: fetch achivements and return achievement object
+        self.worshippers = kwargs.get('Total_Worshippers')
+        self.mastery = kwargs.get('MasteryLevel')
+        self.level = kwargs.get('Level')
+        self.leaves = kwargs.get('Leaves')
+        self.losses = kwargs.get('Losses')
+        self.wins = kwargs.get('Wins')
